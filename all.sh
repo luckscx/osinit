5ba645b94a32fcdd2374d9ccf1522b8610b4e52e
grissomshen <grissomshen@tencent.com>
2023-01-31 10:27:38 +0800

    update all.sh

--- all.sh
+++ all.sh
@@ -8,6 +8,11 @@ yum install htop -y
 
 
 
+echo "Install RigGrep[RG]..."
+
+yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
+yum install ripgrep -y
+
 echo "Install autojump..."
 yum install autojump -y
 
@@ -18,8 +23,3 @@ rm -rf ~/.fzf
 git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
 ~/.fzf/install
 
-
-echo "Install RigGrep[RG]..."
-
-yum-config-manager --add-repo=https://copr.fedorainfracloud.org/coprs/carlwgeorge/ripgrep/repo/epel-7/carlwgeorge-ripgrep-epel-7.repo
-yum install ripgrep -y
